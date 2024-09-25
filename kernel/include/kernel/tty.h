15ffb764f65a46fd1fbadcb35e40bc85354bdee9
Mohammed Anees <pvmohammedanees2003@gmail.com>
2024-09-25 19:27:24 +0000

    Starting of a new journey: Create header for tty

---
+++ kernel/include/kernel/tty.h
@@ -0,0 +1,33 @@
+#ifndef _KERNEL_TTY_H
+#define _KERNEL_TTY_H
+
+#include <stddef.h>
+
+/**
+ * @brief Initializes the terminal with space characters and color.
+ */
+void terminal_initialize(void);
+
+/**
+ * @brief Writes a character to the terminal at the current cursor.
+ *
+ * @param c The character to write to the screen.
+ */
+void terminal_putchar(char c);
+
+/** 
+ * @brief Write a specified number of charaters to the terminal.
+ *
+ * @param data Pointer to the array of character to write.
+ * @param size The size of the array.
+ */
+void terminal_write(const char* data, size_t size);
+
+/**
+ * @brief Write a null terminated string to the terminal.
+ *
+ * @param data Pointer to a null terminated string.
+ */
+void terminal_writestring(const char* data);
+
+#endif
