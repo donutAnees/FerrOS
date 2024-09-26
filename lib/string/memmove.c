7e0bd92cd1f4d8fbe7af632b25a644978ae04f48
Mohammed Anees <pvmohammedanees2003@gmail.com>
2024-09-26 19:44:10 +0530

    lib/stdlib: Implement stdlib functions

---
+++ lib/string/memmove.c
@@ -0,0 +1,15 @@
+#include <string.h>
+
+void* memmove(void* dstptr, const void* srcptr, size_t size) {
+	unsigned char* dst = (unsigned char*) dstptr;
+	const unsigned char* src = (const unsigned char*) srcptr;
+// There is a chance for overlap, therefore we start copying from the end or begin depending on it
+	if (dst < src) {
+		for (size_t i = 0; i < size; i++)
+			dst[i] = src[i];
+	} else {
+		for (size_t i = size; i != 0; i--)
+			dst[i-1] = src[i-1];
+	}
+	return dstptr;
+}
