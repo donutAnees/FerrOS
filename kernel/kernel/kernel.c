9165331ca7b5774a6b01bee8bebd37b9778daba8
Mohammed Anees <pvmohammedanees2003@gmail.com>
2024-10-11 21:05:20 +0530

    Clean up

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -3,10 +3,12 @@
 #include <kernel/tty.h>
 #include <kernel/gdt.h>
 #include <kernel/idt.h>
+#include <kernel/pic.h>
 
 void kernel_main(void) {
 	setup_gdt();
 	setup_idt();
+	PIC_remap(0x20, 0x28);
 	terminal_initialize();
 	printf("Hello, kernel World!\n");
 }
