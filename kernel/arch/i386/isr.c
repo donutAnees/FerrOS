f8c3abd475ada075a0f95f0a35b18067868b66c5
Mohammed Anees <pvmohammedanees2003@gmail.com>
2024-10-13 06:55:52 +0530

    kernel/arch/i386: Implement functions to print keyboard characters to screen
    This commits implements a lot of functions which are required to print
    the characters to the terminal.

--- kernel/arch/i386/isr.c
+++ kernel/arch/i386/isr.c
@@ -1,8 +1,15 @@
 #include <kernel/isr.h>
 #include <kernel/pic.h>
+#include <kernel/ps2_kbd.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <kernel/tty.h>
 #include "io.h"
+
+key_event keyboard_buffer[MAX_KEYB_BUFFER_SIZE];
+uint8_t keyboard_buf_position = 0;
+
+static uint8_t current_state = NORMAL_STATE;
 
 void exception_handler(int num) {
     printf("Hit Exception Handler: %d\n", num);
@@ -11,6 +18,36 @@ void exception_handler(int num) {
 
 void keyboard_irq_handler() {
     uint8_t scancode = inb(0x60);
-    printf("Scancode: %d\n", scancode);
+    if (scancode == 0xE0) {
+        current_state = PREFIX_STATE;
+        // We have a prefix key, so we don't need to do anything with it.
+        // instead, we just return and wait for the next scancode.
+        return;
+    }
+    if (current_state == PREFIX_STATE) {
+        // Store the next part of the scancode, then return to normal state.
+        if (scancode == 0x1D) { // Control
+            keyboard_buffer[keyboard_buf_position].status_mask |= CONTROL_FLAG;
+        } else if (scancode == 0x2A || scancode == 0x36) { // Left or Right Shift
+            keyboard_buffer[keyboard_buf_position].status_mask |= SHIFT_FLAG;
+        } else if (scancode == 0x38) { // Alt
+            keyboard_buffer[keyboard_buf_position].status_mask |= ALT_FLAG;
+        } else if (scancode == 0x1C) { // Enter
+            keyboard_buffer[keyboard_buf_position].status_mask |= ENTER_FLAG;
+        } else {
+            // Do nothing for other keys for now
+        }
+        current_state = NORMAL_STATE;
+    }
+    else  {
+        // If we are in the prefix state, we need to handle the next part of the scancode.
+        // If the scan code is 0x80, it is a key release, where the msb is set to 1.
+        // We need to remove this bit to get the actual scancode.
+        if((scancode & 0x80) == 0) {
+            keyboard_buffer[keyboard_buf_position].code = scancode;
+            keyboard_buf_position = (keyboard_buf_position + 1) % MAX_KEYB_BUFFER_SIZE;
+            terminal_putchar(ps2_kbd_return_ascii(scancode_mapping[scancode]));
+        }
+    }
     PIC_sendEOI(1);
 }
--- kernel/arch/i386/ps2_kbd.c
+++ kernel/arch/i386/ps2_kbd.c
@@ -26,7 +26,66 @@ static void disable_translation() {
 }
 
 void ps2_kbd_init() {
+    // Set the status mask to 0
+    for(uint8_t i = 0; i < MAX_KEYB_BUFFER_SIZE; i++) {
+        keyboard_buffer[i].status_mask = 0;
+    }
     IRQ_clear_mask(PS2_KBD_IRQ_LINE);
     idt_install_irq_handler(keyboard_isr_wrapper);
     idt_install_irq_handler(keyboard_isr_wrapper);
+}
+
+char ps2_kbd_return_ascii(kernel_scancode_set1_t scancode){
+    switch (scancode) {
+        case A: return 'a';
+        case B: return 'b';
+        case C: return 'c';
+        case D: return 'd';
+        case E: return 'e';
+        case F: return 'f';
+        case G: return 'g';
+        case H: return 'h';
+        case I: return 'i';
+        case J: return 'j';
+        case K: return 'k';
+        case L: return 'l';
+        case M: return 'm';
+        case N: return 'n';
+        case O: return 'o';
+        case P: return 'p';
+        case Q: return 'q';
+        case R: return 'r';
+        case S: return 's';
+        case T: return 't';
+        case U: return 'u';
+        case V: return 'v';
+        case W: return 'w';
+        case X: return 'x';
+        case Y: return 'y';
+        case Z: return 'z';
+        case ONE: return '1';
+        case TWO: return '2';
+        case THREE: return '3';
+        case FOUR: return '4';
+        case FIVE: return '5';
+        case SIX: return '6';
+        case SEVEN: return '7';
+        case EIGHT: return '8';
+        case NINE: return '9';
+        case ZERO: return '0';
+        case MINUS: return '-';
+        case EQUAL: return '=';
+        case BACKSPACE: return '\b'; // Backspace character
+        case TAB: return '\t'; // Tab character
+        case LEFT_BRACE: return '[';
+        case RIGHT_BRACE: return ']';
+        case SEMICOLON: return ';';
+        case APOSTROPHE: return '\'';
+        case BACKSLASH: return '\\';
+        case COMMA: return ',';
+        case PERIOD: return '.';
+        case SLASH: return '/';
+        case SPACE: return ' ';
+        default: return '\0'; // Non-printable characters
+    }
 }
